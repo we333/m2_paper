f37b78a4c83af7342f8511afef2cb96e3ac2d5ff
we333 <wantone321@gmail.com>
2018-01-07 21:45:26 +0900

    plt code update

--- plt3d.py
+++ plt3d.py
@@ -6,13 +6,14 @@ import matplotlib as mpl
   
 def draw_bar(labels,title, quants):  
 	colors = []
+	colors.append("#EE1556")
 	colors.append("#41B684")
 	colors.append("#5255DC")
 	colors.append("#D949E4")
 	colors.append("#F88836")
 
 	width = 0.4  
-	ind = np.linspace(0.5,9.5,4)  
+	ind = np.linspace(0.5,9.5,5)  
 	# make a square figure  
 	fig = plt.figure(1)  
 	ax  = fig.add_subplot(111)  
@@ -30,15 +31,31 @@ def draw_bar(labels,title, quants):
 #	plt.show()  
 	plt.savefig(title + ".jpg")  
 	plt.close()
-  
-labels   = ['COLOR base', 'USER base', 'CONTENT base', 'RANDOM']  
-quants_ctr = [0.398, 0.38, 0.35, 0.299] 
-quants_clicks = [29.4, 28.6, 30, 31] 
-quants_statis = [1.33, 0.864, 0.992, 0.71] 
-quants_time = [6.092, 5.576, 6.252, 8.318]  
-titles = ['CTR', 'Pay Per Click', 'Satisfaction', 'Browse Time']
 
-draw_bar(labels, 'CTR', quants_ctr)
-draw_bar(labels, 'Pay Per Click', quants_clicks)
-draw_bar(labels, 'Satisfaction', quants_statis)
-draw_bar(labels, 'Browse Time', quants_time)
+def draw_test_1():
+	labels   = ['COLOR base', 'USER base', 'CONTENT base', 'RANDOM']  
+	quants_ctr = [0.398, 0.38, 0.35, 0.299] 
+	quants_clicks = [29.4, 28.6, 30, 31] 
+	quants_statis = [1.33, 0.864, 0.992, 0.71] 
+	quants_time = [6.092, 5.576, 6.252, 8.318]  
+	titles = ['CTR', 'Pay Per Click', 'Satisfaction', 'Browse Time']
+
+	draw_bar(labels, 'CTR', quants_ctr)
+	draw_bar(labels, 'Pay Per Click', quants_clicks)
+	draw_bar(labels, 'Satisfaction', quants_statis)
+	draw_bar(labels, 'Browse Time', quants_time)
+
+def draw_test_2():
+	labels   = ['COLOR_USER base', 'COLOR base', 'USER base', 'CONTENT base', 'RANDOM']  
+	quants_ctr = [0.39, 0.398, 0.38, 0.35, 0.299] 
+	quants_clicks = [24.4, 29.4, 28.6, 30, 31] 
+	quants_statis = [1.746, 1.33, 0.864, 0.992, 0.71] 
+	quants_time = [5.232, 6.092, 5.576, 6.252, 8.318]  
+	titles = ['CTR', 'Pay Per Click', 'Satisfaction', 'Browse Time']
+
+	draw_bar(labels, 'CTR', quants_ctr)
+	draw_bar(labels, 'Pay Per Click', quants_clicks)
+	draw_bar(labels, 'Satisfaction', quants_statis)
+	draw_bar(labels, 'Browse Time', quants_time)
+
+draw_test_2()
